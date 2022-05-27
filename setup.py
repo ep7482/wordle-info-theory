4e1353090cb10b39683bd55c88bb73175383bba5
ep7 <eduardo42400@gmail.com>
2022-05-26 22:50:32 -0500

    requirements.txt update

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 import setuptools
 
-setuptools.setup(name='wordle_clone', packages=['wordle_clone'])
+with open('requirements.txt', 'r') as f:
+    install_requires = f.read().splitlines()
+
+setuptools.setup(name='wordle_clone', 
+                packages=['wordle_clone'], 
+                install_requires=install_requires)
