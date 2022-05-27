8ac99b49079f69a7894f19dfc9850caa9797992f
ep7 <eduardo42400@gmail.com>
2022-05-26 23:09:55 -0500

    Test purposes

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 
 
 def main():
+    """_summary_
+    """
     pass
+
+
+def test(a, b):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return a,b
 
 if __name__ == '__main__':
     pass
